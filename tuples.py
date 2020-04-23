4dc5de84ab1dcc66020d313e17d629898cf0b5da
Harivansh Mathur <46771504+Harivansh8888@users.noreply.github.com>
2020-04-23 11:20:42 +0530

    accessing tuple item

--- tuples.py
+++ tuples.py
@@ -1,3 +1,7 @@
 name = ("harivansh","neha","maulik") #making a tuple
 
 print(name) #printing a tuple
+
+print(name[1]) #accessing a tuple item
+
+print(name[-1]) #accessing item by negative indexing
