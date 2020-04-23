1048fb13882865107905a7300646553aca649b49
Harivansh Mathur <46771504+Harivansh8888@users.noreply.github.com>
2020-04-23 10:55:20 +0530

    making and accessing list

---
+++ lists.py
@@ -0,0 +1,6 @@
+list1 = ["apple","mango","pineapple"] #making a list
+print(list1) # printing list
+
+print(list1[2]) #accessing an item from list
+
+print(list1[-1]) #accessing by negative indexing
