f5aac193c8b3a7ae24dfe0f489f07b59637a689b
Harivansh Mathur <46771504+Harivansh8888@users.noreply.github.com>
2020-04-23 11:21:43 +0530

    adding item to tuple by converting to list

--- tuples.py
+++ tuples.py
@@ -9,3 +9,9 @@ print(name[-1]) #accessing item by negative indexing
 print(name[0:2]) #range of indexes, 2 is excluded here
 
 print(name[-3:-1]) #range of negative indexes, -1 is excluded here
+
+x = ("apple", "banana", "cherry") #tuple is unchangeable
+y =list(x) #converting tuple into list
+y[1] = "mango" #changing item in list
+x = tuple(y) #converting list back to tuple
+print(x)
