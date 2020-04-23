e5b1eca93ce01afdc9f8289299d03c2932bdbfb5
Harivansh Mathur <46771504+Harivansh8888@users.noreply.github.com>
2020-04-23 10:56:09 +0530

    range of indexes and negative indexes in list

--- lists.py
+++ lists.py
@@ -4,3 +4,12 @@ print(list1) # printing list
 print(list1[2]) #accessing an item from list
 
 print(list1[-1]) #accessing by negative indexing
+
+list2 = ["harivansh","maulik","hridyanshu","ingit","jatin","neha","harshil"]
+print(list2[2:5]) #range of indexes [a:b]- a included, b not included 
+
+print(list2[:3]) #prints from beginning to specified index
+
+print(list2[3:]) #prints from specified index to end
+
+print(list2[-4:-1]) #range of negative indexes
