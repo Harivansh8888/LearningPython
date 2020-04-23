655c7e72f4fb64edc0c9c28b9da52f8f27b96237
Harivansh Mathur <46771504+Harivansh8888@users.noreply.github.com>
2020-04-23 11:21:07 +0530

    using range of indexes

--- tuples.py
+++ tuples.py
@@ -5,3 +5,7 @@ print(name) #printing a tuple
 print(name[1]) #accessing a tuple item
 
 print(name[-1]) #accessing item by negative indexing
+
+print(name[0:2]) #range of indexes, 2 is excluded here
+
+print(name[-3:-1]) #range of negative indexes, -1 is excluded here
