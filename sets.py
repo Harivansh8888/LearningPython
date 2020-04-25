9e5e472c4aff38c4f1a1c3bb191daf04b4f6ed96
Harivansh Mathur <46771504+Harivansh8888@users.noreply.github.com>
2020-04-25 13:35:34 +0530

    length and deleting

--- sets.py
+++ sets.py
@@ -15,3 +15,11 @@ print(set1)
 
 set1.update(["hello","world"]) #adding multiple items at once
 print(set1)
+
+print(len(set1)) #returns length of set
+
+set1.remove("hello") #if item does not exist it will show error
+
+set1.discard("pqr") #if item does not exist it will not show error
+
+set1.pop() #sets are unordered so we don't know what will be removed
