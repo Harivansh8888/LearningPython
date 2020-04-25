3424d4e37761033d316eb7a0110cbe5825a05c84
Harivansh Mathur <46771504+Harivansh8888@users.noreply.github.com>
2020-04-25 14:22:56 +0530

    number of arguments unknown

--- functions.py
+++ functions.py
@@ -7,3 +7,8 @@ def namefunc(name): #passing arguments
     print("Hello " + name)
 namefunc("Harivansh") #calling with arguments
 namefunc("Maulik")
+
+def my_function(*kids): #when number of arguments is unknown
+  print("The youngest child is " + kids[2])
+
+my_function("Emil", "Tobias", "Linus")
