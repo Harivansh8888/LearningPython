56bd189132f34cb7780750329b80fda030dad0e3
Harivansh Mathur <46771504+Harivansh8888@users.noreply.github.com>
2020-04-25 13:51:02 +0530

    if and elif

--- conditions.py
+++ conditions.py
@@ -2,3 +2,10 @@ a = 33
 b = 200
 if b > a: #if implementation
   print("b is greater than a")
+
+p = 10
+q = 10
+if p > q: #if and elif implementation
+    print("p is greater than q")
+elif p == q:
+    print("p and q are equal")
