82029b2a204e7fee6aefb06b661a5923c6706a48
Harivansh Mathur <46771504+Harivansh8888@users.noreply.github.com>
2020-04-25 13:38:24 +0530

    printing keys and values

--- dictionaries.py
+++ dictionaries.py
@@ -15,3 +15,15 @@ print(x)  #x will store i8(value) here
 
 dict1["year"] = "2018" #changing valuess
 print(dict1)
+
+for x in dict1:
+    print(x) #prints the keys
+
+for x in dict1:
+    print(dict1[x]) #prints values
+
+for x in dict1.values():
+    print(x) #prints values
+
+for x,y in dict1.items():
+    print(x,y) #prints keys and values
