ae687b5c5597599214200c877032b32eb74740b1
Harivansh Mathur <46771504+Harivansh8888@users.noreply.github.com>
2020-04-25 14:22:31 +0530

    arguments

--- functions.py
+++ functions.py
@@ -2,3 +2,8 @@ def myfunc(): #creating function
     print("Hello")
 
 myfunc() #calling function
+
+def namefunc(name): #passing arguments
+    print("Hello " + name)
+namefunc("Harivansh") #calling with arguments
+namefunc("Maulik")
