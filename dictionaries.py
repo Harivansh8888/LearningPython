adeb4aaaa86adc1e2a1c488cd96ab5b13fbf3921
Harivansh Mathur <46771504+Harivansh8888@users.noreply.github.com>
2020-04-25 13:38:02 +0530

    accessing and changing values

--- dictionaries.py
+++ dictionaries.py
@@ -9,3 +9,9 @@ dict1 = {
 }
 
 print(dict1)
+
+x =dict1["model"] #alternative --> x=dict1.get("model")
+print(x)  #x will store i8(value) here
+
+dict1["year"] = "2018" #changing valuess
+print(dict1)
