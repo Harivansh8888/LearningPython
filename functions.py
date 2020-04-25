f2a928722fb408bc7ffb30dba537eebbe638bc0a
Harivansh Mathur <46771504+Harivansh8888@users.noreply.github.com>
2020-04-25 14:23:16 +0530

    keyword arguments

--- functions.py
+++ functions.py
@@ -12,3 +12,8 @@ def my_function(*kids): #when number of arguments is unknown
   print("The youngest child is " + kids[2])
 
 my_function("Emil", "Tobias", "Linus")
+
+def my_function(child3, child2, child1): #using keyword arguments
+  print("The youngest child is " + child3)
+
+my_function(child1 = "Emil", child2 = "Tobias", child3 = "Linus")
