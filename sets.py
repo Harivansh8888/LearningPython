5fd902e4152618b4542e1198cc59d43fb2a1163c
Harivansh Mathur <46771504+Harivansh8888@users.noreply.github.com>
2020-04-25 13:35:04 +0530

    adding items

--- sets.py
+++ sets.py
@@ -7,3 +7,11 @@ for x in set1:
     print(x) # unordered in printing
 
 print("def" in set1) #returns True (boolean)
+
+#set items can't be changed but we can add
+
+set1.add("pqr") #adding an item
+print(set1)
+
+set1.update(["hello","world"]) #adding multiple items at once
+print(set1)
