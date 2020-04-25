f09426dedccc1895c1414348e2a420665f0afa42
Harivansh Mathur <46771504+Harivansh8888@users.noreply.github.com>
2020-04-25 13:34:36 +0530

    initial commit

---
+++ sets.py
@@ -0,0 +1,9 @@
+#sets have no index, they are unordered
+
+set1 = {"abc","def","xyz"}
+print(set1)
+
+for x in set1:
+    print(x) # unordered in printing
+
+print("def" in set1) #returns True (boolean)
