37658e055dfbbca025a45f3ea837b1f2e60bb063
Harivansh Mathur <46771504+Harivansh8888@users.noreply.github.com>
2020-04-25 14:08:00 +0530

    break and while

--- loops.py
+++ loops.py
@@ -4,3 +4,11 @@ i = 1
 while i < 6:
   print(i) #six won't be printed
   i += 1 #increasing i value by one
+
+i = 1
+while i < 6:
+  print(i) #will only print 1,2,3
+  if i == 3:
+    break
+  i += 1
+
