5e0f166399d2b46e69123c4d1d1d2fc17c986f1a
Harivansh Mathur <46771504+Harivansh8888@users.noreply.github.com>
2020-04-25 13:53:22 +0530

    nested if else

--- conditions.py
+++ conditions.py
@@ -40,3 +40,11 @@ b = 33
 c = 500
 if a > b or a > c: #OR condition
   print("At least one of the conditions is True")
+
+x = 41
+if x > 10:
+  print("Above ten,")
+  if x > 20:  #nested if implementation
+    print("and also above 20!")
+  else:
+    print("but not above 20.")
