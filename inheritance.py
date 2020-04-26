461f7aa7ec31b3932f7850010a2a19562305d6e6
Harivansh Mathur <46771504+Harivansh8888@users.noreply.github.com>
2020-04-26 13:57:35 +0530

    created child clas

--- inheritance.py
+++ inheritance.py
@@ -6,3 +6,8 @@ class Person: #parent class
     print(self.firstname, self.lastname)
 x = Person("Harivansh", "Mathur")
 x.printname()
+
+class Student(Person): #creating a subclass or child class
+    print("Hi from the child class")
+x = Student("Neha", "Singh")
+x.printname()
