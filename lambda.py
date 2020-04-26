c1a61e07ea9bb384aafc76c1a3355346d237b5b8
Harivansh Mathur <46771504+Harivansh8888@users.noreply.github.com>
2020-04-26 13:20:27 +0530

    lambda inside a function

--- lambda.py
+++ lambda.py
@@ -1,2 +1,7 @@
 x = lambda a,b: a*b*10 #can take multiple arguments
 print(x(2,3))
+
+def myfunc(n):  
+    return lambda a: a*n 
+mydoubler = (myfunc(2)) #using as an ananymous function inside other function
+print(mydoubler)
