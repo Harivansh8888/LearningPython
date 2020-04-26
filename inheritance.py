a30b409f8b46dfe29dda9c850bec4eaae9afc4bd
Harivansh Mathur <46771504+Harivansh8888@users.noreply.github.com>
2020-04-26 13:58:43 +0530

    keeping inheritance of parent's __init__() func

--- inheritance.py
+++ inheritance.py
@@ -15,3 +15,7 @@ x.printname()
 #__init__() function can be used in child class
 #when you add the __init__() function, the child class will no longer inherit the parent's __init__() function
 #the child's __init__() function overrides the inheritance of the parent's __init__() function
+
+class Stu(Person): #creating another child class
+  def __init__(self, fname, lname):
+    Person.__init__(self, fname, lname) #to keep the inheritance of the parent's __init__() function
