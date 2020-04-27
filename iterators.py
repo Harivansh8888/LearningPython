3ed15263498676ddfea7a4ad5c327f5a4bbbb3d3
Harivansh Mathur <46771504+Harivansh8888@users.noreply.github.com>
2020-04-27 10:46:05 +0530

    added some comments

--- iterators.py
+++ iterators.py
@@ -4,3 +4,7 @@ myit = iter(mytuple)
 print(next(myit))
 print(next(myit))
 print(next(myit))
+
+#An iterator is an object that contains a countable number of values
+#An iterator is an object that can be iterated upon, meaning that you can traverse through all the values
+#consist of the methods __iter__() and __next__()
