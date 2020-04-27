af39557cdbc7d54a8104951c6b112bce91e7833e
Harivansh Mathur <46771504+Harivansh8888@users.noreply.github.com>
2020-04-27 13:12:39 +0530

    accessing data from module

--- usingmodule.py
+++ usingmodule.py
@@ -1,1 +1,6 @@
 import mymodule #importing all the data from the mymodule.py
+
+mymodule.greeting(" Harivansh") #accessing that function
+
+a = mymodule.person1["country"] #accessing dictionary created in mymodule.py
+print(a)
