ab9bf445ce950671f57b5d3f3900efed71d610eb
Harivansh Mathur <46771504+Harivansh8888@users.noreply.github.com>
2020-04-27 13:11:44 +0530

    dictionary created

--- mymodule.py
+++ mymodule.py
@@ -1,2 +1,10 @@
 def greeting(name): #creating a function here using it in different program
     print("Hello " + name)
+
+person1 = { #creating a dictionary
+    "fname" : "Harivansh",
+    "lname" : "Mathur",
+    "age" : 19,
+    "country" : "India"
+}
+
