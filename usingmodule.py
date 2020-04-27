940da6e33ad70fe6fb44956c2ec90dd93157644c
Harivansh Mathur <46771504+Harivansh8888@users.noreply.github.com>
2020-04-27 13:13:10 +0530

    renaming a module

--- usingmodule.py
+++ usingmodule.py
@@ -4,3 +4,8 @@ mymodule.greeting(" Harivansh") #accessing that function
 
 a = mymodule.person1["country"] #accessing dictionary created in mymodule.py
 print(a)
+
+"""
+import mymodule as mx -----> to rename a module
+a = mx.person1["age"]
+"""
