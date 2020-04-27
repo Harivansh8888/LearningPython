32265aa3ea6f1df518468a4d295e8744bb26dbbc
Harivansh Mathur <46771504+Harivansh8888@users.noreply.github.com>
2020-04-27 10:46:26 +0530

    iterating string

--- iterators.py
+++ iterators.py
@@ -8,3 +8,13 @@ print(next(myit))
 #An iterator is an object that contains a countable number of values
 #An iterator is an object that can be iterated upon, meaning that you can traverse through all the values
 #consist of the methods __iter__() and __next__()
+
+mystr = "banana" #strings are also iterable objects
+myit1 = iter(mystr)
+
+print(next(myit1))
+print(next(myit1))
+print(next(myit1))
+print(next(myit1))
+print(next(myit1))
+print(next(myit1))
