82e399f9f9830317cdbad8b30f75b8c4a6c2393c
Harivansh Mathur <46771504+Harivansh8888@users.noreply.github.com>
2020-04-28 11:24:28 +0530

    some print statements

--- dates.py
+++ dates.py
@@ -3,3 +3,7 @@ import datetime
 x = datetime.datetime.now() #working with date as object
 
 y =datetime.datetime(2020, 5, 3) #creating a date object
+
+print(x)
+print(x.year) #prints current year
+print(x.strftime("%A")) #prints the current day
