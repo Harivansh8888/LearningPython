d65b4f7d78ef478116cb89b5bd68f08927f40ee6
Harivansh Mathur <46771504+Harivansh8888@users.noreply.github.com>
2020-04-28 11:52:51 +0530

    multiple exceptions

--- exceptions.py
+++ exceptions.py
@@ -2,3 +2,10 @@ try:
     print(x) #x is not defined here
 except:
     print("An exception occured")
+
+try:
+    print(y)
+except NameError:
+    print("y is not defined") #many exceptions with a single try
+except:
+    print("Some other exception occured")
